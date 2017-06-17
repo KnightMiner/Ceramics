a661ac54ac0d63c32a7e2cf31285fd96ec6f9fa9
KnightMiner <knightminer4@gmail.com>
2017-06-16 22:09:12 -0500

    Allow itemblocks of enum blocks to define a nice tooltip automatically

--- src/main/java/knightminer/ceramics/items/ItemBlockEnum.java
+++ src/main/java/knightminer/ceramics/items/ItemBlockEnum.java
@@ -1,14 +1,20 @@
 package knightminer.ceramics.items;
 
+import java.util.List;
+
 import javax.annotation.Nonnull;
 
 import knightminer.ceramics.blocks.IBlockEnum;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemColored;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.translation.I18n;
 
+@SuppressWarnings("deprecation")
 public class ItemBlockEnum extends ItemColored {
 
 	private PropertyEnum<?> prop;
@@ -18,7 +24,17 @@ public class ItemBlockEnum extends ItemColored {
 		this.prop = block.getMappingProperty();
 	}
 
-	@SuppressWarnings("deprecation")
+	@Override
+	public void addInformation(@Nonnull ItemStack stack, @Nonnull EntityPlayer playerIn, @Nonnull List<String> tooltip, boolean advanced) {
+		if(I18n.canTranslate(this.getUnlocalizedName(stack) + ".tooltip")) {
+			tooltip.add(TextFormatting.GRAY.toString() + I18n.translateToLocal(this.getUnlocalizedName(stack) + ".tooltip"));
+		}
+		else if(I18n.canTranslate(super.getUnlocalizedName(stack) + ".tooltip")) {
+			tooltip.add(TextFormatting.GRAY.toString() + I18n.translateToLocal(super.getUnlocalizedName(stack) + ".tooltip"));
+		}
+		super.addInformation(stack, playerIn, tooltip, advanced);
+	}
+
 	@Nonnull
 	@Override
 	public String getUnlocalizedName(@Nonnull ItemStack stack) {
