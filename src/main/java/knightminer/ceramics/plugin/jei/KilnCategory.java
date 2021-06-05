6bc00a12b2fb255f95a9faa492615cc50927ce2f
KnightMiner <knightminer4@gmail.com>
2021-06-04 20:41:09 -0400

    Update JEI support to match new furnace features

--- src/main/java/knightminer/ceramics/plugin/jei/KilnCategory.java
+++ src/main/java/knightminer/ceramics/plugin/jei/KilnCategory.java
@@ -1,5 +1,8 @@
 package knightminer.ceramics.plugin.jei;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import knightminer.ceramics.Ceramics;
 import knightminer.ceramics.Registration;
@@ -17,9 +20,9 @@ import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.ForgeI18n;
 
 @MethodsReturnNonnullByDefault
@@ -34,8 +37,8 @@ public class KilnCategory implements IRecipeCategory<KilnRecipe> {
   private final IDrawable background;
   private final IDrawable icon;
   private final IDrawableAnimated animatedFlame;
-  private final IDrawableAnimated arrow;
   private final String localizedName;
+  private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
 
   KilnCategory(IGuiHelper guiHelper) {
     // elements
@@ -45,7 +48,22 @@ public class KilnCategory implements IRecipeCategory<KilnRecipe> {
     // animations
     IDrawableStatic staticFlame = guiHelper.createDrawable(RECIPE_GUI_VANILLA, 82, 114, 14, 14);
     this.animatedFlame = guiHelper.createAnimatedDrawable(staticFlame, 300, StartDirection.TOP, true);
-    this.arrow = guiHelper.drawableBuilder(RECIPE_GUI_VANILLA, 82, 128, 24, 17).buildAnimated(100, StartDirection.LEFT, false);
+    this.cachedArrows = CacheBuilder.newBuilder().maximumSize(25L).build(new CacheLoader<Integer, IDrawableAnimated>() {
+      @Override
+      public IDrawableAnimated load(Integer cookTime) {
+        return guiHelper.drawableBuilder(RECIPE_GUI_VANILLA, 82, 128, 24, 17).buildAnimated(cookTime, StartDirection.LEFT, false);
+      }
+    });
+  }
+
+  /** Gets the arrow for the given recipe */
+  protected IDrawableAnimated getArrow(KilnRecipe recipe) {
+    int cookTime = recipe.getCookTime();
+    if (cookTime <= 0) {
+      cookTime = 100;
+    }
+
+    return this.cachedArrows.getUnchecked(cookTime);
   }
 
 
@@ -96,14 +114,34 @@ public class KilnCategory implements IRecipeCategory<KilnRecipe> {
   @Override
   public void draw(KilnRecipe recipe, MatrixStack matrixStack, double mouseX, double mouseY) {
     this.animatedFlame.draw(matrixStack, 1, 20);
-    this.arrow.draw(matrixStack, 24, 18);
+    IDrawableAnimated arrow = this.getArrow(recipe);
+    arrow.draw(matrixStack, 24, 18);
+    this.drawExperience(recipe, matrixStack);
+    this.drawCookTime(recipe, matrixStack);
+  }
 
+  /** Draws the recipe experience info */
+  protected void drawExperience(KilnRecipe recipe, MatrixStack matrixStack) {
     float experience = recipe.getExperience();
     if (experience > 0.0F) {
-      String experienceString = I18n.format("gui.jei.category.smelting.experience", experience);
-      FontRenderer fontRenderer = Minecraft.getInstance().fontRenderer;
-      int stringWidth = fontRenderer.getStringWidth(experienceString);
-      fontRenderer.drawString(matrixStack, experienceString, (float)(this.background.getWidth() - stringWidth), 0, 0xFF808080);
+      TranslationTextComponent experienceString = new TranslationTextComponent("gui.jei.category.smelting.experience", experience);
+      Minecraft minecraft = Minecraft.getInstance();
+      FontRenderer fontRenderer = minecraft.fontRenderer;
+      int stringWidth = fontRenderer.getStringPropertyWidth(experienceString);
+      fontRenderer.func_243248_b(matrixStack, experienceString, (this.background.getWidth() - stringWidth), 0, 0xFF808080);
+    }
+  }
+
+  /** Draws the recipe cook time info */
+  protected void drawCookTime(KilnRecipe recipe, MatrixStack matrixStack) {
+    int cookTime = recipe.getCookTime();
+    if (cookTime > 0) {
+      int cookTimeSeconds = cookTime / 20;
+      TranslationTextComponent timeString = new TranslationTextComponent("gui.jei.category.smelting.time.seconds", cookTimeSeconds);
+      Minecraft minecraft = Minecraft.getInstance();
+      FontRenderer fontRenderer = minecraft.fontRenderer;
+      int stringWidth = fontRenderer.getStringPropertyWidth(timeString);
+      fontRenderer.func_243248_b(matrixStack, timeString, (this.background.getWidth() - stringWidth), 45, 0xFF808080);
     }
   }
 }
