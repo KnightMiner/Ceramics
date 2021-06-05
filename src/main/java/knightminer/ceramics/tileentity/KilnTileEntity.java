7fb93add14762725809ef52eb8b296077ac8dc4c
KnightMiner <knightminer4@gmail.com>
2021-06-04 20:10:56 -0400

    Fix fuel lasting too long in the kiln
    Was inconsistent with smokers and blast furnaces

--- src/main/java/knightminer/ceramics/tileentity/KilnTileEntity.java
+++ src/main/java/knightminer/ceramics/tileentity/KilnTileEntity.java
@@ -4,6 +4,7 @@ import knightminer.ceramics.Registration;
 import knightminer.ceramics.container.KilnContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.AbstractFurnaceTileEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -18,6 +19,11 @@ public class KilnTileEntity extends AbstractFurnaceTileEntity {
     return new TranslationTextComponent("container.ceramics.kiln");
   }
 
+  @Override
+  protected int getBurnTime(ItemStack fuel) {
+    return super.getBurnTime(fuel) / 2;
+  }
+  
   @Override
   protected Container createMenu(int id, PlayerInventory player) {
     return new KilnContainer(id, player, this, this.furnaceData);
